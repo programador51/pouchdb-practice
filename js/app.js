e6e4c26a5e29f4f6866daa8452c680485f88c8b6
programador51 <gskitcat@gmail.com>
2021-08-15 13:28:23 -0500

    feat: Seccion 95 - Todo a PWA

--- js/app.js
+++ js/app.js
@@ -2,6 +2,10 @@
 
   'use strict';
 
+  if(navigator.serviceWorker){
+    navigator.serviceWorker.register('/sw.js');
+  }
+
   var ENTER_KEY = 13;
   var newTodoDom = document.getElementById('new-todo');
   var syncDom = document.getElementById('sync-wrapper');
